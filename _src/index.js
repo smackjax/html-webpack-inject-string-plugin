a3426e8618686eda6cf69d43851cd8da1e6d25f5
smackjax <smackjax@gmail.com>
2019-11-20 15:24:58 -0500

    Initial commit

---
+++ _src/index.js
@@ -0,0 +1,89 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = class HtmlWebpackInjectStringPlugin {
+    // NOTE: Fires on htmlWebpackPlugin 'beforeEmit' event
+    // --- Defaults ---
+    // append: false
+    // prepend: true
+    // replace: false
+    // search: ""
+    // inject: ""
+    // newline: { before: true, after: true }
+    // dev: false
+
+    
+    constructor(options) {
+      this.options = options;
+    }
+
+    apply(compiler) {
+        if(this.options.dev) console.log("====== html-webpack-inject-string-plugin ======");
+
+        compiler.hooks.compilation.tap('HtmlWebpackInjectStringPlugin', (compilation) => {
+
+            // Static Plugin interface |compilation |HOOK NAME | register listener 
+            HtmlWebpackPlugin.getHooks(compilation).beforeEmit.tapAsync(
+                'HtmlWebpackInjectStringPlugin',
+                (data, cb) => {
+                    // Validate
+                    if(typeof this.options.search != 'string' || !this.options.search) throw Error("html-webpack-inject-string-plugin: { 'search' } must be a string and cannot be empty!");
+                    if(typeof this.options.inject != 'string') throw Error("html-webpack-inject-string-plugin: { 'inject' } must be a string!");
+
+                    // Searches for string
+                    if(this.options.dev) console.log("Searching template for string: ", this.options.search);                    
+                    const searchStringIndex = data.html.indexOf(this.options.search);
+
+                    // Found string
+                    if (searchStringIndex > -1) {
+                        if(this.options.dev) console.log("Search string found! Injecting string: ", this.options.inject);
+
+                        // Check for newlines
+                        let stringToInject = this.options.inject;
+                        if (this.options.newline.before) {
+                            if(this.options.dev) console.log("Adding newline before injection string"); 
+                            stringToInject = '\r\n' + stringToInject;
+                        }
+                        if (this.options.newline.after) {
+                            if(this.options.dev) console.log("Adding newline after injection string");          
+                            stringToInject = stringToInject + '\r\n';
+                        }
+
+                        
+                        // Kudos to jAndy
+                        // https://stackoverflow.com/a/4364902
+                        const splitString = data.html.split(this.options.search).splice(0, 0, this.options.search);
+
+                        // In the interest of being versatile, you can prepend, append and replace at the same time if you want. 
+                        if(replace) {
+                            if(this.options.dev) console.log("Replacing injection string");
+                            splitString[1] = this.options.inject;
+                        }
+                        if(append) {
+                            if(this.options.dev) console.log("Appending injection string");
+                            splitString.splice(2, 0, this.options.inject);
+                        }
+                        if(prepend) {
+                            if(this.options.dev) console.log("Prepending injection string");
+                            splitString.splice(1, 0, this.options.inject);
+                        }
+                        
+                        // Build new string
+                        if(this.options.dev) console.log("Bulding new string from " + splitString.length + " elements");
+                        data.html = splitString.join('');
+                    } else {
+                        if(this.options.dev) console.log("Couldn't find string matching search");
+                    }
+    
+                    // Keep on keeping on
+                    if(this.options.dev) console.log("===============================================");
+                    cb(null, data);
+                }
+            )
+        })
+    }
+   };
+
+
+
+
+module.exports = HtmlWebpackInjectStringPlugin;
---
+++ readme.md
@@ -0,0 +1,90 @@
+# HTML WEBPACK INJECT STRING PLUGIN
+## What's this?
+A dead simple plugin that searches each output of html-webpack-plugin for a custom string(like '</body>') and prepends, replaces, or appends a another custom string by injecting it, then returning the completed template.
+
+## Config default options 
+```javascript
+// For example, searching for '</body>'
+{
+    // String to search for
+    search: "",
+    // String to inject
+    inject: ""
+    
+    // Injects before found string
+    prepend: true
+    // Replaces found string with injection
+    replace: false
+    // Injects after found string
+    append: false
+    
+    // Adds 'r\n\' before or after injection string
+    newline: { before: true, after: true }
+    // Enables console.log messages
+    dev: false
+}
+```
+
+## Usage 
+```javascript
+const htmlWebpackInjectStringPlugin = require(html-webpack-inject-string-plugin);
+
+{
+/* EXAMPLE BASE CODE:
+    <div id='replace-me'></div>
+</body>
+*/
+
+{ // USAGE EXAMPLE 1
+    plugins: [
+        
+        // Simple
+        new htmlWebpackInjectStringPlugin({
+            search: "</body>",
+            inject: "<script>alert('injected')</script>"
+            // Defaults to prepending before search string
+        }),
+        /* Results:
+            <div id='replace-me'></div>
+
+            <script>alert('injected')</script>
+
+        </body>
+        */
+    ]
+}
+ 
+{ // USAGE EXAMPLE 2
+    plugins: [
+        // More complex
+        new htmlWebpackInjectStringPlugin({
+            search: "<div id='replace-me'></div",
+            inject: "<a>I'm a replacement</a>",
+            prepend: false,
+            replace: true,
+            append: true,
+            newline: {
+                before: false,
+                after: false
+            }
+        }),
+        /* Results:
+            <a>I'm a replacement</a><a>I'm a replacement</a> <--- replaced and appended, so two injected
+        </body>
+        */
+    ]
+}
+```
+
+## Wait, isn't this a little dangerous?
+Yep! 
+
+## So why would I use it?
+Because sometimes it's just incredibly helpful. 
+For instance I needed to inject a browser-sync script into each html-webpack file with that contained '</body>'.
+I didn't want the boiler plate of having to add it individually and conditionally(only in development) to each page. 
+So I made this plugin to add it automatically and only where it was needed. 
+
+## I really like this! But it could be better. Can I contribute?
+Of course! I just ask that any direct pull requests against here be as unopinionated as possible.
+If you have a great idea that's more opinionated, feel free to fork this as a starting point.
